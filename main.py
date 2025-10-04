4994a675a590bd5ce597f46b4dce4ba1ec2dd48a
Kittiphat Leesombatwathana <79010876+LeeIsBadK@users.noreply.github.com>
2025-10-04 21:36:19 +0700

    Feat: project structure

---
+++ main.py
@@ -0,0 +1,48 @@
+ 
+import asyncio
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
+from app.route.router import router as slip_router
+from db.mongo import close_mongo_connection, connect_to_mongo
+ 
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # Startup
+    await connect_to_mongo()
+    yield
+    # Shutdown
+    close_mongo_connection()
+    
+app = FastAPI(title="Eiei Marketplace Reservation Management", lifespan=lifespan)
+list = [ 
+       
+        "http://localhost:3000"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],       
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"],          
+    allow_headers=["*"],          
+)
+ 
+ 
+
+app.include_router(slip_router, prefix="/slip", tags=["Reservations"])
+
+
+async def serve_fastapi():
+    config = uvicorn.Config(app, host="0.0.0.0", port=7003)
+    server = uvicorn.Server(config)
+    await server.serve()
+    
+async def main():
+    await asyncio.gather(
+        serve_fastapi(),
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
